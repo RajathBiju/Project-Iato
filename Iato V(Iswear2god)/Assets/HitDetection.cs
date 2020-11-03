c83d6ad69899cd91b489bc49765cb68d008e7b95
Nitikk <nitik9.arora@gmail.com>
2020-11-04 00:31:51 +0530

    Revert "Merge branch 'main' of https://github.com/RajathBiju/Project-Iato into main"
    This reverts commit 9ffae0827a5796151886402c6be8f7006890eda1, reversing
    changes made to 1de709ec6394389b1d5519d8a58477578ea70a2a.

--- Iato V(Iswear2god)/Assets/HitDetection.cs
+++ Iato V(Iswear2god)/Assets/HitDetection.cs
@@ -7,11 +7,6 @@ public class HitDetection : MonoBehaviour
     public GameObject Cube1, Cube2;
     public EnemyMovement Movement;
 
-<<<<<<< HEAD
-=======
-    public GameObject PunchHeavy, PunchMid, PunchLight;
-
->>>>>>> c9b8d7fbc2253a6f999257fbe72a9c1aced7da72
     /*w2private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "AttackNode")
@@ -25,28 +20,9 @@ public class HitDetection : MonoBehaviour
         Movement = GetComponent<EnemyMovement>();
     }
 
-<<<<<<< HEAD
     public void HIT()
     {
         if(Movement.InAttackRange)
         Debug.Log("Hit");
-=======
-    public void HITHand1()
-    {
-        if (Movement.InAttackRange)
-        {
-            Debug.Log("Hit");
-            Destroy(Instantiate(PunchMid, Cube1.transform.position, transform.rotation), 1.5f);
-        }
-    }
-
-    public void HITHand2()
-    {
-        if (Movement.InAttackRange)
-        {
-            Debug.Log("Hit");
-            Destroy(Instantiate(PunchMid, Cube2.transform.position, transform.rotation), 1.5f);
-        }
->>>>>>> c9b8d7fbc2253a6f999257fbe72a9c1aced7da72
     }
 }
